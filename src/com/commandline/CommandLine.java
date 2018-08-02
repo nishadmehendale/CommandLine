377739dd2e2c4d438840b1d0b050102f01d14bf9
nishadmehendale <nishadmehendale@gmail.com>
2018-08-03 00:33:14 +0530

    Add files via upload

---
+++ src/com/commandline/CommandLine.java
@@ -0,0 +1,11 @@
+package com.commandline;
+
+public class CommandLine {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of 3 numbers is "
+				+ (Integer.parseInt(args[0]) + Integer.parseInt(args[1]) + Integer.parseInt(args[2])));
+
+	}
+
+}
